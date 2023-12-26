c7a802fb3c308ff2d92526e069472da18e8e409b
Florian Chanson <f.chanson@barbablanka.com>
2023-12-26 16:18:23 +0100

    Added publish code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    `java-library`
+    `maven-publish`
 }
 
 group = "dev.blackoutburst"
@@ -11,4 +13,22 @@ repositories {
 
 kotlin {
     jvmToolchain(11)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Blackoutburst/Thrones")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
