2a086df837457ef821f6cf4fa842cf1f0bfff1ba
Florian Chanson <f.chanson@barbablanka.com>
2023-12-27 22:16:54 +0100

    Updated build.gradle to move onto custom publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,24 +31,6 @@ buildscript {
     }
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Blackoutburst/Thrones")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
-        }
-    }
-}
-
 tasks.withType<DokkaTask>().configureEach {
     pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
         footerMessage = "(c) 2024 Florian \"Blackoutburst\" Chanson"
@@ -66,5 +48,40 @@ tasks.withType<DokkaTask>().configureEach {
                 Visibility.PACKAGE,
             )
         )
+    }
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+
+            pom {
+                name.set("Thrones")
+                description.set("Lightweight logger api")
+                url.set("https://github.com/Blackoutburst/Thrones")
+
+                licenses {
+                    license {
+                        name.set("The Unlicense")
+                        url.set("https://unlicense.org")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("blackoutburst")
+                        name.set("Florian \"Blackoutburst\" Chanson")
+                        email.set("blackoutburst@hotmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Blackoutburst/Thrones.git")
+                    developerConnection.set("scm:git:ssh://github.com:Blackoutburst/Thrones.git")
+                    url.set("https://github.com/Blackoutburst/Thrones/")
+                }
+            }
+        }
     }
 }
